6ca9cc6d1f310993fd09fafe56223ee2f99d7022
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-02-25 17:31:23 +0600

    [ADDED] images and resources, mobile menu added

--- constant/index.js
+++ constant/index.js
@@ -1,14 +1,96 @@
-const navlinks = {
+export const navlinks = {
   textLinks: [
     {
-      label: "",
-      path: "",
+      label: "About",
+      path: "/about",
+      subMenu: [
+        {
+          label: "The game",
+          path: "/the-game",
+        },
+        {
+          label: "Team",
+          path: "/team",
+        },
+        {
+          label: "Ranking",
+          path: "/ranking",
+        },
+        {
+          label: "Lore/Back Story",
+          path: "/lorestory",
+        },
+        {
+          label: "Factions Summary",
+          path: "/facsummary",
+        },
+        {
+          label: "Roadmap 2",
+          path: "/assets/resources/Roadmap1.png",
+        },
+        {
+          label: "Tokenomics",
+          path: "/assets/resources/katanainutokenomics.pdf",
+        },
+        {
+          label: "Whitepaper",
+          path: "/assets/resources/katanainuwhitepaper.pdf",
+        },
+      ],
+    },
+    {
+      label: "Marketplace",
+      path: "https://kainu.io/",
+    },
+    {
+      label: "Play",
+      path: "https://play.katanainu.com/",
+    },
+    {
+      label: "Faq",
+      path: "/faq",
+    },
+    {
+      label: "Blog",
+      path: "/blog",
     },
   ],
-  imageLinks: [
+  socialLinks: [
     {
-      path: "",
-      imageUrl: "",
+      path: "https://twitter.com/katanainu",
+      imageUrl: "/assets/icons/twitter.svg",
+      imageUrlC: "/assets/icons/twitter-c.svg",
+      name: "twitter",
+    },
+    {
+      path: "https://discord.com/invite/katanainu",
+      imageUrl: "/assets/icons/discord.svg",
+      imageUrlC: "/assets/icons/discord-c.svg",
+      name: "discord",
+    },
+    {
+      path: "https://www.instagram.com/katanainu",
+      imageUrl: "/assets/icons/instagram.svg",
+      imageUrlC: "/assets/icons/instagram-c.svg",
+      name: "instagram",
+    },
+    {
+      path: "https://www.facebook.com/profile.php?id=100069383490342",
+      imageUrl: "/assets/icons/facebook.svg",
+      imageUrlC: "/assets/icons/facebook-c.svg",
+      name: "facebook",
+    },
+    {
+      path: "https://www.reddit.com/r/KatanaInu_Official/?share_id=LNKrnOGAnht0zb6-V3i5m&utm_medium=ios_app&utm_name=iossmf&utm_source=share&utm_term=14",
+      imageUrl: "/assets/icons/reddit.svg",
+      imageUrlC: "/assets/icons/reddit-c.svg",
+      name: "reddit",
+    },
+    {
+      path: "https://t.me/katanainu",
+      imageUrl: "/assets/icons/telegram.svg",
+      imageUrlC: "/assets/icons/telegram-c.svg",
+      name: "telegram",
     },
   ],
 };
