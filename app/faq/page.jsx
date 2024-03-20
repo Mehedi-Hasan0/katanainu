a4e3730cfa5526eb6ccfdc291c23fc77f6a3c9f5
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-03-20 17:30:20 +0600

    faq text alignment fixed

--- app/faq/page.jsx
+++ app/faq/page.jsx
@@ -11,10 +11,10 @@ export default function Faq() {
     <main className="pt-36 sm:pt-44 pb-0 md:pt-52 lg:pt-60 2xl:pt-64 md:pb-20 bg-[url('/assets/images/team_bg.webp')] bg-cover">
       <section className="main-container">
         <div className="flex flex-col justify-center items-center">
-          <h5 className="uppercase font-jost font-bold text-gradient text-xl sm:text-2xl md:text-3xl tracking-wider mb-3">
+          <h5 className="uppercase font-jost font-bold text-gradient text-lg sm:text-2xl md:text-3xl tracking-wider sm:mb-3">
             Frequently Asked Questions
           </h5>
-          <p className="text-sm md:text-base text-[#f8f9fa] font-medium my-3 pb-6 sm:pb-8 md:pb-10 text-center max-w-[850px]">
+          <p className="text-base text-[#f8f9fa] font-light md:font-medium my-3 pb-6 sm:pb-8 md:pb-10 text-center max-w-[850px]">
             Below we have provided our summarized frequently asked questions. If
             you have additional questions, concerns, improvement proposals or
             you would like to cooperate with us, please get in touch with us by
@@ -32,7 +32,7 @@ export default function Faq() {
           >
             {faqData.map((data, i) => (
               <AccordionItem value={`item-${i + 1}`} key={i}>
-                <AccordionTrigger className="font-jost data-[state=open]:border-[#f3a511] border-b border-dashed border-transparent px-6 py-5 text-sm md:text-base font-normal">
+                <AccordionTrigger className="font-jost data-[state=open]:border-[#f3a511] border-b border-dashed border-transparent px-6 py-5 text-sm md:text-base font-normal text-left">
                   {data.title}
                 </AccordionTrigger>
                 <AccordionContent className="font-poppins pt-2 px-4 text-sm md:text-base font-normal text-[#dbdbdb]">
